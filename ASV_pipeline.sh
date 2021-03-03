c7a6069d69e625851a8215b3b5c47f655b7a3725
ilevantis <ilevantis@users.noreply.github.com>
2021-03-03 12:05:08 +0000

    made indentation and spacing more readable

--- ASV_pipeline.sh
+++ ASV_pipeline.sh
@@ -25,7 +25,9 @@
 # 7) GNU parallel
 
 #set -e
-source ${HOME}/.bashrc
+#source ${HOME}/.bashrc
+
+
 # some functions
 length_stats(){
 python - << EOF
@@ -57,6 +59,7 @@ with open('${2}_distribution.txt', 'a+') as F:
 EOF
 }
 
+
 run_QC_single_pair(){
 # Argumnets:
 # 1) Forward primer
@@ -71,92 +74,103 @@ run_QC_single_pair(){
 
 # run first cutadapt
 fileset=`basename ${7}`
-sample=`echo ${fileset}|rev|cut -d '.' -f1|rev`
+sample=`echo ${fileset} | rev | cut -d '.' -f1 | rev`
 echo "Processing  sample ${sample}"
 echo -e "\tRemoving primers"
-if [[ -s ${5}/${sample}.1.fastq.gz ]]
-then
+if [[ -s ${5}/${sample}.1.fastq.gz ]]; then
     echo -e "File ${5}/${sample}.1.fastq.gz exist and is not empty... Skipping"
 else
     cutadapt -g "${1}" -G "${2}" -a "${3}" -A "${4}" \
-    -o ${5}/${sample}.1.fastq.gz -p ${5}/${sample}.2.fastq.gz \
-    -m $9 --match-read-wildcards -q 25 --trim-n -n 2 --untrimmed-output \
-    ${5}/untrimmed.${sample}.fastq --untrimmed-paired-output \
-    ${5}/untrimmed.paired.${sample}.fastq ${7}_R1.fastq.gz \
-    ${7}_R2.fastq.gz > ${5}/${sample}.log1
+      -o ${5}/${sample}.1.fastq.gz -p ${5}/${sample}.2.fastq.gz \
+      -m $9 --match-read-wildcards -q 25 --trim-n -n 2 --untrimmed-output \
+      ${5}/untrimmed.${sample}.fastq --untrimmed-paired-output \
+      ${5}/untrimmed.paired.${sample}.fastq ${7}_R1.fastq.gz \
+      ${7}_R2.fastq.gz > ${5}/${sample}.log1
 fi
+
 # Merge reads
 #echo "Running pear"
 #pear -f ${5}/${sample}.1.fastq.gz -r ${5}/${sample}.2.fastq.gz \
 #-o ${5}/${sample}_pear -q 25 -t $9 -s 2 > ${5}/${sample}_pear.log
 #echo "Checking if any adapters remain"
-if [[ -s ${5}/${sample}_pear.assembled.fastq ]]
-then
+if [[ -s ${5}/${sample}_pear.assembled.fastq ]]; then
     echo -e "\t\tFile ${5}/${sample}_pear.assembled.fastq exist and is not empty... Skipping"
 else
     echo -e "\tMerging reads"
     pear -f ${5}/${sample}.1.fastq.gz -r ${5}/${sample}.2.fastq.gz \
-    -o ${5}/${sample}_pear -q 25 -t $9 -s 2 > ${5}/${sample}_pear.log
+      -o ${5}/${sample}_pear -q 25 -t $9 -s 2 > ${5}/${sample}_pear.log
 fi
+
 # check if adapters still there
 echo -e "\tIdentifying remnant primers"
 seqkit -j ${8} locate -d -p "${1}" ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.fwd
 seqkit -j ${8} locate -d -p "${2}" ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.rev
+
 # cut the adapter if they remain
-if [[ $(wc -l <${5}/${sample}.fwd) -ge 2 || $(wc -l <${5}/${sample}.rev) -ge 2 ]]
-then
+if [[ $(wc -l <${5}/${sample}.fwd) -ge 2 || $(wc -l <${5}/${sample}.rev) -ge 2 ]]; then
     echo -e "\t\tRemnant primers found, attempting to remove them"
     cutadapt -a "$3" -m $9 -n 2 --too-short-output ${5}/${sample}.short.fastq \
-    -o ${5}/${sample}.3trimmed.fastq ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.log2
+      -o ${5}/${sample}.3trimmed.fastq ${5}/${sample}_pear.assembled.fastq > ${5}/${sample}.log2
     cutadapt -g "${1}" -n 2 -o ${5}/${sample}.5trimmed.fastq \
-    ${5}/${sample}.3trimmed.fastq > ${outdir}/${sample}.log3
+      ${5}/${sample}.3trimmed.fastq > ${outdir}/${sample}.log3
 else
     echo -e "\t\tNo remnant primers found"
     cp ${5}/${sample}_pear.assembled.fastq ${5}/${sample}.3trimmed.fastq
 fi
+
 echo "Dereplicating"
 usearch -fastx_uniques ${5}/${sample}.3trimmed.fastq -fastaout \
-${5}/${sample}.trimmed.derep.fasta -sizeout
+  ${5}/${sample}.trimmed.derep.fasta -sizeout
+
 echo "Estimating length distributions"
 length_stats ${5}/${sample}.trimmed.derep.fasta ${5}/${6}
+
 echo "Estimating stats for all the steps"
 seqkit -j ${8} stats ${5}/*${sample}*.fa* >> ${5}/${6}.stats
+
 # dereplicate
 echo -e "\tRemoving duplicate sequences"
-if [[ -s ${5}/${sample}.trimmed.derep.fasta ]]
-then
+if [[ -s ${5}/${sample}.trimmed.derep.fasta ]]; then
     echo -e "\t\tFile${5}/${sample}.trimmed.derep.fasta exist and is not empty... Skipping"
+
 else
-    fs=`du -b ${5}/${sample}.3trimmed.fastq|cut -f1`
+    fs=`du -b ${5}/${sample}.3trimmed.fastq | cut -f1`
     fs=$(( fs / 1000000000 ))
-    if [[ "fs" -gt 3 ]]
-    then
+
+    if [[ "fs" -gt 3 ]]; then
         p=`python -c "from math import ceil; print(int(ceil($fs/3))"`
         seqkit -j ${8} split2 ${5}/${sample}.3trimmed.fastq -p ${p} -f \
-        -O split_trimmed
-        for i in split_trimmed/*part_*
-        do
+          -O split_trimmed
+
+        for i in split_trimmed/*part_* ; do
             usearch -fastx_uniques ${i} -fastaout split_trimmed/${i}_uniq \
             -sizeout -threads ${8}
         done
+
         cat split_trimmed/*_uniq > tmp
         usearch -fastx_uniques tmp -fastaout ${5}/${sample}.trimmed.derep.fasta \
-        -sizein -sizeout -threads ${8}
+          -sizein -sizeout -threads ${8}
+
     else
         usearch -fastx_uniques ${5}/${sample}.3trimmed.fastq -fastaout \
-        ${5}/${sample}.trimmed.derep.fasta -sizeout -threads ${8}
+          ${5}/${sample}.trimmed.derep.fasta -sizeout -threads ${8}
     fi
+
 fi
+
 # get lenghth distributions
 length_stats ${5}/${sample}.trimmed.derep.fasta ${5}/${6}
 # get stats for all the steps
 seqkit -j ${8} stats ${5}/*${sample}*.fa* >> ${5}/${6}.stats
+
 # run a fastqc
 echo "Running fastqc"
 fastqc ${5}/${sample}.3trimmed.fastq -o ${5}
 #fastqc ${5}/${sample}.3trimmed.fastq -o ${5}
 echo -e "\n\n"
+
 }
+
 
 prog() {
     # Progress bar, courtesy of ilkkachu (stackoverflow)
@@ -165,14 +179,19 @@ prog() {
     printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*";
 }
 
+
 fastx_relabel() {
 # 1. *.lengthfilter.fastq file
 i=${1}
 prefix=`basename ${i%%.lengthfilter.fastq}`_
-awk -v l="$prefix" 'BEGIN { cntr = 0 } /^@[A-Z][0-9]{5}:.{3}:*:*[:space:]*/ { \
-cntr++ ; print l, cntr } !/^@[A-Z][0-9]{5}:.{3}:*:*[:space:]*/ \
-{ print $0 }' > ${i%%.fastq}_relabel.fastq
+awk -v l="$prefix" '
+  BEGIN {cntr = 0}
+  /^@[A-Z][0-9]{5}:.{3}:*:*[:space:]*/{cntr++; print l, cntr}
+  !/^@[A-Z][0-9]{5}:.{3}:*:*[:space:]*/{print $0}
+  ' > ${i%%.fastq}_relabel.fastq
 }
+
+
 # Get the command line arguments
 file_list=$1
 ADAPTER_FWD=$2
@@ -185,49 +204,53 @@ max_len=$8
 outdir=${prefix}_output${primer_name}
 min_read_len=$(( (min_len / 2) - 20 ))
 usearch_min_size=${9}
+
+
 echo `date`
 echo -e "executing $0 $@\n"
+
 # Process the primers and get reverse complements
 Adapter1rc=`echo $ADAPTER_FWD | tr 'ACGTYRSWKMBDHV' 'TGCARYSWMKVHDB' | rev`
 Adapter2rc=`echo $ADAPTER_REV | tr 'ACGTYRSWKMBDHV' 'TGCARYSWMKVHDB' | rev`
+
 # create output folder
 mkdir -p ${outdir}
 echo "" > ${outdir}/${prefix}.stats
 echo "" > ${outdir}/${prefix}_distribution.txt
+
 # run QC in all pairs
-while read line
-do
+while read line ; do
     run_QC_single_pair ${ADAPTER_FWD} ${ADAPTER_REV} ${Adapter2rc} \
-    ${Adapter1rc} ${outdir} ${prefix} ${line} ${cpus} ${min_read_len}
+      ${Adapter1rc} ${outdir} ${prefix} ${line} ${cpus} ${min_read_len}
 done <${file_list}
 
 # Trim the reads to expected length
 echo "Trimming all samples between ${min_len} and ${max_len}"
-n=`ls ${outdir}/*3trimmed.fastq| wc -l`
+n=`ls ${outdir}/*3trimmed.fastq | wc -l`
 perc=0
-for i in ${outdir}/*3trimmed.fastq
-do
+for i in ${outdir}/*3trimmed.fastq ; do
     let perc++
     prog $(( (perc * 100) / n ))
-    cat ${i} | seqkit -j ${cpus} seq -m ${min_len} \
-    -M ${max_len} >${i%%3trimmed.fastq}lengthfilter.fastq
+    cat ${i} \
+    | seqkit -j ${cpus} seq -m ${min_len} -M ${max_len} \
+    > ${i%%3trimmed.fastq}lengthfilter.fastq
 done
 
-echo -e "\nConcatenating all resulting files"
 # Concatenate all resulting files
+echo -e "\nConcatenating all resulting files"
 cat ${outdir}/*.lengthfilter.fastq > ${outdir}/all.lengthfilter.fastq
 
 # Dereplicate on full set reporting size of identical sequences
 echo -e "\nDereplicating full set reporting size of identical sequences"
 vsearch --derep_fulllength  ${outdir}/all.lengthfilter.fastq -sizein -sizeout \
--relabel Uniq -output ${outdir}/vs_all_lengthfilter.fasta
+  -relabel Uniq -output ${outdir}/vs_all_lengthfilter.fasta
 
 # execute denoising in full file
 echo -e "\nDenoising"
 usearch -unoise3 ${outdir}/vs_all_lengthfilter.fasta -zotus \
-${outdir}/all_lengthfilter_zotus_${usearch_min_size}.fasta \
--minsize ${usearch_min_size} -tabbedout \
-${outdir}/allzotus_lengthfilter_${usearch_min_size}.txt
+  ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.fasta \
+  -minsize ${usearch_min_size} -tabbedout \
+  ${outdir}/allzotus_lengthfilter_${usearch_min_size}.txt
 
 # Rename to Zotu
 echo -e "\n Renaming sequences"
@@ -235,14 +258,13 @@ sed -i "s/>O/>Zo/g" ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.fasta
 
 # reannotate each sample
 echo -e "\n Re-annotate each sample"
-n=`ls ${outdir}/*lengthfilter.fastq| wc -l`
+n=`ls ${outdir}/*lengthfilter.fastq | wc -l`
 perc=0
-for i in ${outdir}/*lengthfilter.fastq
-do
+for i in ${outdir}/*lengthfilter.fastq ; do
     let perc++
     prog $(( (perc * 100) / n ))
     usearch -fastx_relabel ${i} -prefix `basename ${i%%.lengthfilter.fastq}`_ \
-    -fastqout ${i%%.fastq}_relabel.fastq -keep_annots
+      -fastqout ${i%%.fastq}_relabel.fastq -keep_annots
     #fastx_relabel ${i}
 done
 
@@ -250,7 +272,7 @@ done
 # Make database with the joined file
 echo -e "\n\nCreating usearch database"
 usearch -makeudb_usearch ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.fasta \
--output ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb
+  -output ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb
 
 # Concatenating relabeled sequences
 echo -e "\nConcatenating relabeled sequences"
@@ -258,33 +280,33 @@ cat ${outdir}/*lengthfilter_relabel.fastq > ${outdir}/all.lengthfilter_relabel.f
 
 # get file_size
 echo -e "\nGenerating ZOTU/ASV  table"
-file_size=`du -b ${outdir}/all.lengthfilter_relabel.fastq|cut -f1`
+file_size=`du -b ${outdir}/all.lengthfilter_relabel.fastq | cut -f1`
 file_size=$(( file_size / 1000000000 ))
-if [[ "$file_size" -gt 3 ]]
-    then
-        # split file into 3GB chunks
-        p=`python -c "from math import ceil; print(int(ceil(${file_size}/3)))"`
-        seqkit split2 ${outdir}/all.lengthfilter_relabel.fastq -p ${p} -f \
-        -O split
-        # execute in smaller files
-        for i in split/*part_*
-        do
-            dir=`dirname ${i}`
-            part=`basename ${i}|rev| cut -d'.' -f 2|rev`
-            usearch -otutab ${i}  -sample_delim _ \
-            -zotus ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb \
-            -otutabout ${dir}/${part}_zotutab_${usearch_min_size}.txt \
-            -mapout ${dir}/${part}zmap_${usearch_min_size}.txt
-        done
-        # merge them
-        # merge the individual otu tables
-        files=`echo split/*zotutab_${usearch_min_size}.txt|tr ' ' ','`
-        usearch -otutab_merge ${files} -output ${outdir}/allzotus_lengthfilter_${usearch_min_size}.txt
-    else
+if [[ "$file_size" -gt 3 ]]; then
+    # split file into 3GB chunks
+    p=`python -c "from math import ceil; print(int(ceil(${file_size}/3)))"`
+    seqkit split2 ${outdir}/all.lengthfilter_relabel.fastq -p ${p} -f -O split
+
+    # execute in smaller files
+    for i in split/*part_* ; do
+        dir=`dirname ${i}`
+        part=`basename ${i} | rev | cut -d'.' -f 2 | rev`
+        usearch -otutab ${i}  -sample_delim _ \
+          -zotus ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb \
+          -otutabout ${dir}/${part}_zotutab_${usearch_min_size}.txt \
+          -mapout ${dir}/${part}zmap_${usearch_min_size}.txt
+    done
+
+    # merge them
+    # merge the individual otu tables
+    files=`echo split/*zotutab_${usearch_min_size}.txt | tr ' ' ','`
+    usearch -otutab_merge ${files} -output ${outdir}/allzotus_lengthfilter_${usearch_min_size}.txt
+
+else
     # execute full
     # Created the Zotu table with the renamed samples
     usearch -otutab ${outdir}/all.lengthfilter_relabel.fastq  -sample_delim _ \
-    -zotus ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb -otutabout \
-    ${outdir}/all_${primer_name}zotutab_${usearch_min_size}.txt -mapout \
-    ${outdir}/all_${primer_name}zmap_${usearch_min_size}.txt
+      -zotus ${outdir}/all_lengthfilter_zotus_${usearch_min_size}.udb -otutabout \
+      ${outdir}/all_${primer_name}zotutab_${usearch_min_size}.txt -mapout \
+      ${outdir}/all_${primer_name}zmap_${usearch_min_size}.txt
 fi
